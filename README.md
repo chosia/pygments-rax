6c02d8fe5487ef78ec09f49200cf25d1a8889080
gosia <gosia@codersco.com>
2017-10-27 11:45:53 +0200

    It seems I committed some cat code. Fixing this.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pygments-rax
 Pygments lexer for Rax
+
+## Installation
+pip install . --upgrade
--- pygmentsrax/lexer.py
+++ pygmentsrax/lexer.py
@@ -1,4 +1,4 @@
-;'''UY|@Qqq1 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # Copyright (c) 2017 Coders Co.
 # Licensed under the MIT License (https://opensource.org/licenses/MIT)
 
